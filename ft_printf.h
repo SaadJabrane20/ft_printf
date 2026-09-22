92266e6aae02a547d29fefcb0ebbc227982ba1c8
SaadJabrane20 <sjabrane20@gmail.com>
2026-09-22 18:51:23 +0000

    fixed norme errors

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sajabran <sajabran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/21 14:55:42 by sajabran          #+#    #+#             */
-/*   Updated: 2026/09/21 19:45:32 by sajabran         ###   ########.fr       */
+/*   Updated: 2026/09/22 19:50:57 by sajabran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t		ft_putstr(char *s);
 int			ft_put_hex(unsigned int n, const char *base);
 const char	*check_base(char c);
 int			ft_print_pointer(void *ptr);
-int			parser(const char *format, va_list args);
+int			parser(char format, va_list args);
 int			ft_printf(const char *format, ...);
 int			ft_putnbr_unsigned(unsigned int n);
 #endif
